48a7eea6238af189fcad70148c090ada16ab43ee
syedalihax <syedalihax@gmail.com>
2026-09-03 11:16:33 -0700

    userSchema updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,8 +3,12 @@ const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
     userName: { type: String, unique: [true, "user name should be unique"], required: [true, "user name is required."] },
     email: { type: String, unique: true, required: [true, "email is required."] },
-    role: { type: String, required: true, enum: ["admin", "user"] },
-    password: { type: String, required: [true, "password is required."] }
+    role: { type: String, required: true, enum: ["admin", "user"] , default : "user"},
+    password: { type: String, required: [true, "password is required."] , select : false}
+},
+{
+    timestamps : true,
+    versionKey : false
 })
 
 const UserModel = mongoose.model("userModel", userSchema)
