21af0bc965505f6c97a6d4d01df5a9c1dd9c103a
syedalihax <Syedalihax@gmail.com>
2026-09-07 19:02:14 +0500

    register login page file created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Auth from "./components/Auth"
+import Navbar from "./components/Navbar"
+
+const App = () => {
+    return (
+        <>
+        <Navbar />
+        <Auth />
+        </>
+        
+    )
+}
+
+export default App
