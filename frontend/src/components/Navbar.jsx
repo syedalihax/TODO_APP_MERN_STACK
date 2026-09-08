9a91d15cb3737b68322eefac01a82e7d1a26620d
syedalihax <syedalihax@gmail.com>
2026-09-08 16:41:15 +0500

    navbar redesign

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,12 +2,15 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav className='text-white flex justify-between px-30 items-center w-full h-20 bg-blue-800'>
+    <nav className=' text-black flex justify-between px-16 items-center w-full h-20 bg-app-bg select-none'>
+      <div className="flex justify-center gap-2 items-center">
+        <img className="w-15" src="../../public/notes.png" alt="" />
         <h1 className="text-3xl font-bold ">KEEP NOTES</h1>
-        <ul className="mr-10 flex justify-center gap-5 text-2xl items-center font-semibold">
-            <li className="cursor-pointer hover:text-blue-100 select-none"><Link to={"/"}>Home</Link></li>
-            <li className="cursor-pointer hover:text-blue-100 select-none"><Link to={"/signup"}>SignUp</Link></li>
-            <li className="cursor-pointer hover:text-blue-100 select-none"><Link to={"/login"}>Login</Link></li>
+      </div>
+        <ul className="mr-10 flex justify-center gap-5 text-xl items-center font-semibold">
+            <li className="cursor-pointer hover:scale-105 select-none"><Link to={"/"}>Home</Link></li>
+            <li className="cursor-pointer hover:scale-105 select-none"><Link to={"/login"}>Login</Link></li>
+            <button className="bg-[#964adc] hover:bg-btn rounded-2xl text-app-bg py-2 px-4 cursor-pointer select-none"><Link to={"/signup"}>Start for Free</Link></button>
         </ul>
     </nav>
   )
