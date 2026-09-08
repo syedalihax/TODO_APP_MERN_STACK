0bfb71984bdaa224b7f268bad7948e65ea27b591
syedalihax <Syedalihax@gmail.com>
2026-09-08 14:01:06 +0500

    navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between px-10 items-center w-full h-20 bg-blue-800'>
+    <nav className='text-white flex justify-between px-30 items-center w-full h-20 bg-blue-800'>
         <h1 className="text-3xl font-bold ">KEEP NOTES</h1>
-        <ul className="flex justify-center gap-5 text-2xl items-center font-semibold">
-            <li className="cursor-pointer hover:text-blue-100 select-none">SignUp</li>
-            <li className="cursor-pointer hover:text-blue-100 select-none">Login</li>
+        <ul className="mr-10 flex justify-center gap-5 text-2xl items-center font-semibold">
+            <li className="cursor-pointer hover:text-blue-100 select-none"><Link to={"/"}>Home</Link></li>
+            <li className="cursor-pointer hover:text-blue-100 select-none"><Link to={"/signup"}>SignUp</Link></li>
+            <li className="cursor-pointer hover:text-blue-100 select-none"><Link to={"/login"}>Login</Link></li>
         </ul>
     </nav>
   )
