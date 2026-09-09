53e5afe5cee61740024fff0eab617accf98e6535
syedalihax <syedalihax@gmail.com>
2026-09-09 06:39:03 +0500

    clear alle

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,20 +5,7 @@ const Home = () => {
   return (
     <>
       <Navbar />
-      <div className="pt-20 flex justify-center bg-app-bg select-none">
-        <div className="w-[52%] pl-15 pr-5 flex flex-col gap-10 justify-center">
-          <h1 className=" text-6xl font-medium">Unlock Effortless Productivity. Your Notes, Organized.</h1>
-          <p className="text-xl text-[#9249d6]">The simple, powerful web application to capture, organize, and manage your tasks. Start in seconds.</p>
-          <button className="bg-primary text-app-bg w-fit mx-auto px-6 py-3 text-2xl rounded-xl cursor-pointer hover:bg-[#f62854]">
-            <Link to={'/signup'}>
-              Create Your First Note
-            </Link>
-          </button>
-        </div>
-        <div className="w-[47%]">
-          <img className="w-full mr-auto" src="/hero.jpg" alt="" />
-        </div>
-      </div>
+      
     </>
   )
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -27,63 +27,7 @@ const Signup = () => {
     return (
 
         <>
-            <div className='fixed h-screen overflow-hidden select-none'>
-                <Nav />
-                <div className='flex w-full h-full justify-center pt-15'>
-                    <div className='border border-[#00000030] flex w-[80%] h-[80%] rounded-2xl overflow-hidden shadow-xl shadow-[#00000070]'>
-
-                        <div className='w-[50%] h-full flex flex-col pt-10 gap-2.5 items-center px-15'>
-                            <div className=' mr-auto border rounded-full w-8 h-8 p-0.5 cursor-pointer hover:bg-gray-200'>
-
-                                <Link to='/'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round">
-                                        <line x1="19" y1="12" x2="5" y2="12" />
-                                        <polyline points="12 19 5 12 12 5" />
-                                    </svg>
-                                </Link>
-                            </div>
-                            <div className=' my-1'>
-                                <h1 className=' text-center text-4xl font-medium '>Join Keep Notes</h1>
-                                <p className='text-xl font-normal '>Create Your Account</p>
-                                {/* <p className='text-center p-1 text-red-700 border border-red-700 rounded-md text-sm font-normal '>failed to register hello hello wow ow oweir motehr chod</p> */}
-                            </div>
-                            <form className='flex flex-col  w-full' onSubmit={(e) => { submitHandler(e) }}>
-                                <h1 className='text-lg my-2'>Name:</h1>
-                                <input
-                                    value={userName}
-                                    onChange={(e) => { setUserName(e.target.value) }}
-                                    className='border-2  rounded-xl text-lg py-2 px-3 focus:outline-none  focus:ring-0 font-medium '
-                                    type="text"
-                                    placeholder='Full Name' />
-                                <h1 className='text-lg my-2'>Email:</h1>
-                                <input
-                                    value={email}
-                                    onChange={(e) => { setEmail(e.target.value) }}
-                                    className='border-2  rounded-xl text-lg py-2 px-3 focus:outline-none  focus:ring-0 font-medium '
-                                    type="text"
-                                    placeholder=' Email Address' />
-                                <h1 className='text-lg my-2'>Password:</h1>
-                                <input
-                                    value={password}
-                                    onChange={(e) => { setPassword(e.target.value) }}
-                                    className='border-2  rounded-xl text-lg py-2 px-3 focus:outline-none  focus:ring-0 font-medium '
-                                    type="text"
-                                    placeholder=' Strong Password' />
-                                <button className='text-2xl text-white bg-primary w-fit px-6 py-2 rounded-md my-5 hover:bg-primary/80 ml-auto cursor-pointer' type='submit'>Register</button>
-                            </form>
-                            <div className='w-full'>
-                                <p className='w-full text-left'>Already have an account? <u className='cursor-pointer hover:text-primary text-lg'><Link to='/login'>Login</Link></u></p>
-                            </div>
-                        </div>
-
-                        <div className='bg-[url("/reg.jpg")] bg-cover bg-center w-[50%] h-full'>
-
-                        </div>
-
-                    </div>
-
-                </div>
-            </div >
+        
         </>
     )
 }
