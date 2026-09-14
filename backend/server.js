2dadbb825bedc1adbb0e47cbe620b9f951ddd870
syedalihax <syedalihax@gmail.com>
2026-09-14 13:35:29 +0500

    server connection bug fix

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const startServer = async () => {
     try {
         await connectDb();
 
-        app.listen(port, () => {
+        app.listen(port, '0.0.0.0', () => {
             console.log(`-- server is running on port ${port} --`);
         });
 
