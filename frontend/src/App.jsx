c13841fa26d35eeedab116a3b1efd61c9cb631fa
syedalihax <syedalihax@gmail.com>
2026-09-22 12:58:44 +0500

    add the protected dasboard route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,17 @@ import Signup from "./pages/Signup"
 import Login from "./pages/Login"
 import Home from "./pages/Home"
 import Dashboard from "./pages/Dashboard"
+import Protected from "./components/Protected"
 
 const App = () => {
     return (
         <div className="relative">
-            
+
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/dashboard" element={<Protected><Dashboard /></Protected>} />
             </Routes>
         </div>
 
